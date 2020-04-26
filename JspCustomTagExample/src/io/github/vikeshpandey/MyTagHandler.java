1da4a3c74be6fa1ebc01e218d3381b9d3cbf0bd7
Pandey <pandvike@38f9d3698f00.ant.amazon.com>
2020-04-26 02:52:43 +0200

    jspcustomtag brought in

---
+++ JspCustomTagExample/src/io/github/vikeshpandey/MyTagHandler.java
@@ -0,0 +1,25 @@
+package io.github.vikeshpandey;
+
+import java.util.Calendar;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class MyTagHandler extends TagSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public int doStartTag() throws JspException {
+		JspWriter out = pageContext.getOut();// returns the instance of JspWriter
+		try {
+			out.print(Calendar.getInstance().getTime());// printing date and time using JspWriter
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return SKIP_BODY;// will not evaluate the body content of the tag
+	}
+}
